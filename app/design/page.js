1b21bd091ed5291b614aed10accc388cc4d3107b
SePuede <stale4669@gmail.com>
2025-02-28 23:40:51 +0100

    updates

--- app/design/page.js
+++ app/design/page.js
@@ -5,6 +5,42 @@ import Link from 'next/link';
 import Image from 'next/image'
 import img from '@/images/white.png'
 
+
+/*
+            <div className='dynamic-rectangle' style={{ width }}>
+                <div className="logo m-5">
+                    <Image
+                        src={img}
+                        alt="logo"
+                        width={30}
+                        height={30}
+                        className="logo-image"
+                    />
+                    <Link href={"/"}>
+                        <p className='text-2xl font-medium'>Portfolio</p>
+                    </Link>
+                </div>
+            </div>
+
+
+
+
+            <div className='container'>
+                <div className='mininav mt-5'>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('500px')}>XS</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('750px')}>S</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1000px')}>M</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1250px')}>L</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1500px')}>XL</button>
+                </div>
+                <div className='left-div'>
+                    <button className='left-arr'>Click</button>
+                </div>
+               
+                
+            </div>
+*/
+
 export default function Design() {
     const [width, setWidth] = useState(null);
 
@@ -18,30 +54,13 @@ export default function Design() {
 
     return(
         <>
-            <div>
-                <div className='mininav mt-5'>
-                    <button className='mininav-btn' onClick={() => handleButtonClick('500px')}>XS</button>
-                    <button className='mininav-btn' onClick={() => handleButtonClick('750px')}>S</button>
-                    <button className='mininav-btn' onClick={() => handleButtonClick('1000px')}>M</button>
-                    <button className='mininav-btn' onClick={() => handleButtonClick('1250px')}>L</button>
-                    <button className='mininav-btn' onClick={() => handleButtonClick('1500px')}>XL</button>
-                </div>
-                <button className='right-arr'>Click</button>
-                <div className='dynamic-rectangle' style={{ width }}>
-                <div className="logo m-5">
-                        <Image
-                            src={img}
-                            alt="logo"
-                            width={30}
-                            height={30}
-                            className="logo-image"
-                        />
-                        <Link href={"/"}>
-                            <p className='text-2xl font-medium'>Portfolio</p>
-                        </Link>
-                    </div>
-                </div>
+            <div className='container'>
+                <div className='header'>su</div>
+                <div className='menu'>middle</div>
+                <div className='footer'>giu</div>
             </div>
+
+            
         </>
     );
 }
