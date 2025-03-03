baf434ed6babe99e3df68f90a3d439bcafb167fc
SePuede <stale4669@gmail.com>
2025-03-03 23:24:20 +0100

    updates

--- app/design/page.js
+++ app/design/page.js
@@ -55,10 +55,34 @@ export default function Design() {
     return(
         <>
             <div className='container'>
-                <div className='header'>su</div>
-                <div className='menu'>middle</div>
-                <div className='footer'>giu</div>
+                <div className='mininav mt-5'>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('500px')}>XS</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('750px')}>S</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1000px')}>M</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1250px')}>L</button>
+                    <button className='mininav-btn' onClick={() => handleButtonClick('1500px')}>XL</button>
+                </div>
+
+                <div className='left-div'>
+                    <button className='left-arr'>➡</button> 
+                </div>
+
+                <div className='dynamic-rectangle' style={{ width }}>
+                <div className="logo m-5">
+                    <Image
+                        src={img}
+                        alt="logo"
+                        width={30}
+                        height={30}
+                        className="logo-image"
+                    />
+                    <Link href={"/"}>
+                        <p className='text-2xl font-medium'>Portfolio</p>
+                    </Link>
+                </div>
             </div>
+            </div>
+
 
             
         </>
